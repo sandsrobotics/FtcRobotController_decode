e25244591260c654a12e992c421591c9d3f72459
Tom Kroon <soccer@kroon.ws>
2025-10-20 12:04:49 -0400

    adding sdk v11 and cleaning out old opmodes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/T3_AutoExample.java
@@ -0,0 +1,128 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.teamcode.parts.bulkread.BulkRead;
+import org.firstinspires.ftc.teamcode.parts.drive.Drive;
+import org.firstinspires.ftc.teamcode.parts.intake2.Intake2;
+import org.firstinspires.ftc.teamcode.parts.positionsolver.PositionSolver;
+import org.firstinspires.ftc.teamcode.parts.positionsolver.settings.PositionSolverSettings;
+import org.firstinspires.ftc.teamcode.parts.positiontracker.PositionTracker;
+import org.firstinspires.ftc.teamcode.parts.positiontracker.hardware.PositionTrackerHardware;
+import org.firstinspires.ftc.teamcode.parts.positiontracker.pinpoint.Pinpoint;
+import org.firstinspires.ftc.teamcode.parts.positiontracker.settings.PositionTrackerSettings;
+import java.text.DecimalFormat;
+import java.util.function.Function;
+import om.self.ezftc.core.Robot;
+import om.self.ezftc.utils.Constants;
+import om.self.ezftc.utils.Vector3;
+import om.self.task.core.Group;
+import om.self.task.other.TimedTask;
+import static om.self.ezftc.utils.Constants.tileSide;
+
+@Config
+@Autonomous(name="32859 Auto Example ", group="32859")
+public class T3_AutoExample extends LinearOpMode{
+    public Function<Vector3, Vector3> transformFunc;
+    public Vector3 customStartPos;
+    public boolean shutdownps;
+    PositionSolver positionSolver;
+    PositionTracker pt;
+    Vector3 startPosition;
+    Pinpoint odo;
+    Intake2 intake;
+    //  DASHBOARD VARIABLES (static public)
+    static public int shortDelay = 1000;
+    static public int midDelay = 2000;
+    static public int longDelay = 3000;
+    public static int maxDelay = 3000;
+    /**************************/
+    public int startDelay;
+    private int parkPosition;
+    Vector3 fieldStartPos;
+
+    public void initAuto(){
+        transformFunc = (v) -> v;
+        fieldStartPos = new Vector3(14.375, -62, -90);
+    }
+
+    private Vector3 tileToInchAuto(Vector3 tiles){
+        return Constants.tileToInch(transformFunc.apply(tiles));
+    }
+
+    private Vector3 tileToInchAutoNoZ(Vector3 tiles){ return Constants.tileToInch(transformFunc.apply(tiles)).withZ(tiles.Z); }
+
+    public Vector3 fieldToTile(Vector3 p){
+        return new Vector3(p.X / tileSide, p.Y / tileSide, p.Z);
+    }
+
+    @Override
+    public void runOpMode() {
+        long start;
+        initAuto();
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
+        TelemetryPacket packet = new TelemetryPacket();
+        Telemetry dashboardTelemetry = dashboard.getTelemetry();
+        Robot robot = new Robot(this);
+        Drive drive = new Drive(robot);
+        new BulkRead(robot);
+        intake = new Intake2(robot, "Autonomous");
+
+        PositionTrackerSettings pts = new PositionTrackerSettings(AxesOrder.XYZ, false,
+                100, new Vector3(2, 2, 2), fieldStartPos);
+        pt = new PositionTracker(robot, pts, PositionTrackerHardware.makeDefault(robot));
+        odo = new Pinpoint(pt);
+        pt.positionSourceId = Pinpoint.class;
+        positionSolver = new PositionSolver(drive); // removed so it won't rotate 90deg clockwise
+        DecimalFormat df = new DecimalFormat("#0.0");
+
+        robot.init();
+
+        while (!isStarted()) {
+            sleep(50);
+        }
+        odo.setPosition(fieldStartPos);
+        robot.start();
+
+        if(shutdownps) positionSolver.triggerEvent(Robot.Events.STOP);
+
+        // Setting up group container, task queue, and setting positionSolver target
+        Group container = new Group("container", robot.taskManager);
+        TimedTask autoTasks = new TimedTask("auto task", container);
+
+        TestAuto(autoTasks);
+
+        while (opModeIsActive()) {
+            start = System.currentTimeMillis();
+            robot.run();
+            dashboardTelemetry.addData("position", pt.getCurrentPosition());
+            telemetry.addData("position", pt.getCurrentPosition());
+            telemetry.addData("tile position", fieldToTile(pt.getCurrentPosition()));
+            telemetry.addData("time", System.currentTimeMillis() - start);
+            dashboardTelemetry.update();
+            telemetry.update();
+        }
+        robot.stop();
+    }
+
+    public void TestAuto(TimedTask autoTasks) { // from 14273
+        // Positions to travel in Auto
+        Vector3 start = new Vector3(14.375, -62, -90);
+        Vector3 launch = new Vector3(14.375, -50, -45);
+        // Reset and Get Ready.
+        autoTasks.addStep(() -> positionSolver.setSettings(PositionSolverSettings.loseSettings));
+        // First Sample to ObservationZone.
+        positionSolver.addMoveToTaskEx(start, autoTasks);
+        positionSolver.addMoveToTaskEx(launch, autoTasks);
+
+    }
+}
